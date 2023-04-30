58a0c49b82fba06a6d33a25cafc48b77d8105be5
Mehul <gulatimehul2000@gmail.com>
2023-04-30 17:29:29 +0530

    Navbar added

--- frontend/public/assets/js/sidebarmenu.js
+++ frontend/public/assets/js/sidebarmenu.js
@@ -1,48 +1,48 @@
+// @ts-nocheck
 // ==============================================================
 // Auto select left navbar
 // ============================================================== 
+
+import $ from 'jquery';
 $(function () {
-     var url = window.location + "";
-        var path = url.replace(window.location.protocol + "//" + window.location.host + "/", "");
-        var element = $('ul#sidebarnav a').filter(function() {
-            return this.href === url || this.href === path;// || url.href.indexOf(this.href) === 0;
-        });
+    var url = window.location + "";
+    var path = url.replace(window.location.protocol + "//" + window.location.host + "/", "");
+    var element = $('ul#sidebarnav a').filter(function () {
+        return this.href === url || this.href === path;// || url.href.indexOf(this.href) === 0;
+    });
 
-        element.parentsUntil(".sidebar-nav").each(function (index)
-        {
-            if($(this).is("li") && $(this).children("a").length !== 0)
-            {
-                $(this).parent("ul#sidebarnav").length === 0
-                    ? $(this).addClass("active")
-                    : $(this).addClass("selected");
-            }
-            else if(!$(this).is("ul") && $(this).children("a").length === 0)
-            {
-                $(this).addClass("selected");
-            }
-            else if($(this).is("ul")){
-                $(this).show();
-            }
-        });
+    element.parentsUntil(".sidebar-nav").each(function (index) {
+        if ($(this).is("li") && $(this).children("a").length !== 0) {
+            $(this).parent("ul#sidebarnav").length === 0
+                ? $(this).addClass("active")
+                : $(this).addClass("selected");
+        }
+        else if (!$(this).is("ul") && $(this).children("a").length === 0) {
+            $(this).addClass("selected");
+        }
+        else if ($(this).is("ul")) {
+            $(this).show();
+        }
+    });
 
-    element.addClass("active"); 
+    element.addClass("active");
     $('#sidebarnav a').on('click', function (e) {
-        
-            if (!$(this).hasClass("active")) {
-                // hide any open menus and remove all other classes
-                $("ul", $(this).parents("ul:first")).removeClass("in");
-                $("a", $(this).parents("ul:first")).removeClass("active");
-                
-                // open our new menu and add the open class
-                $(this).next("ul").addClass("in");
-                $(this).addClass("active");
-                
-            }
-            else if ($(this).hasClass("active")) {
-                $(this).removeClass("active");
-                $(this).parents("ul:first").removeClass("active");
-                $(this).next("ul").removeClass("in");
-            }
+
+        if (!$(this).hasClass("active")) {
+            // hide any open menus and remove all other classes
+            $("ul", $(this).parents("ul:first")).removeClass("in");
+            $("a", $(this).parents("ul:first")).removeClass("active");
+
+            // open our new menu and add the open class
+            $(this).next("ul").addClass("in");
+            $(this).addClass("active");
+
+        }
+        else if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).parents("ul:first").removeClass("active");
+            $(this).next("ul").removeClass("in");
+        }
     })
     $('#sidebarnav >li >a.has-arrow').on('click', function (e) {
         e.preventDefault();
@@ -115,11 +115,11 @@ $(function () {
             $.each(o, function (e) {
                 $("body").removeClass(o[e]);
             }),
-                $("body").addClass(e),
-                (i = "skin"),
-                (a = e),
-                "undefined" != typeof Storage ? localStorage.setItem(i, a) : window.alert("Please use a modern browser to properly view this template!"),
-                !1
+            $("body").addClass(e),
+            (i = "skin"),
+            (a = e),
+            "undefined" != typeof Storage ? localStorage.setItem(i, a) : window.alert("Please use a modern browser to properly view this template!"),
+            !1
         );
     }
     (i = void ("undefined" != typeof Storage || window.alert("Please use a modern browser to properly view this template!"))) && $.inArray(i, o) && a(i),
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,215 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import Footer from './component/layout/Footer';
+// @ts-nocheck
+import React, { useEffect } from "react";
+import "./App.css";
+import Footer from "./component/layout/Footer";
 // import Header from './component/layout/Header';
-import {  BrowserRouter as Router,Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-import store from './store';
-import { loadUser } from './actions/userAction';
-import Login from './component/User/Login';
-import Team from './component/Team/Team';
-import AddNewCompany from './component/Company/Admin/AddNewCompany';
-import ForgotPassword from './component/User/ForgotPassword';
-import ResetPassword from './component/User/ResetPassword';
-import UserProfile from './component/Profile/UserProfile';
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import Login from "./component/User/Login";
+import Team from "./component/Team/Team";
+import AddNewCompany from "./component/Company/Admin/AddNewCompany";
+import ForgotPassword from "./component/User/ForgotPassword";
+import ResetPassword from "./component/User/ResetPassword";
+import UserProfile from "./component/Profile/UserProfile";
+import Developer from "./component/Developer/Developer";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+const drawerWidth = 240;
 
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 function App() {
   useEffect(() => {
     store.dispatch(loadUser());
-  }, [])
+  }, []);
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    if (!open) setOpen(true);
+    else setOpen(false);
+  };
+
   return (
-    <Router id="main-wrapper" className="App">
-      {/* <Header /> */}
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/team" element={<Team />} />
-        <Route exact path="/add-new-company" element={<AddNewCompany />} />
-        <Route exact path="/forgot-password" element={<ForgotPassword />} />
-        <Route exact path="/reset-password/:token" element={<ResetPassword />} />
-        <Route exact path="/profile" element={<UserProfile />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader></DrawerHeader>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
