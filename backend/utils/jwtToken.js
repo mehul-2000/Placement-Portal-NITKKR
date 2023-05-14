5d9878c47a5313861e2db92f062fbc40122c89ad
Ishant Goyal <goyalishant001@gmail.com>
2023-05-14 22:41:32 +0530

    dep

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -12,7 +12,7 @@ const sendToken = (user, statusCode, res) => {
             secure: true, 
             httpOnly: false, 
             sameSite: 'None',
-            domain: 'vercel.app'
+            domain: '.placement-portal-nitkkr.vercel.app'
         })
     };
     res.status(statusCode).cookie('token', token, options).json({
