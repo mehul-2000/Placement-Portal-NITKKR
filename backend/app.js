9f3ee5b2d9114ee0df25ebfac78ed79314fa14f7
Ishant Goyal <goyalishant001@gmail.com>
2023-05-14 19:48:07 +0530

    dep

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,8 @@ app.use("/api/notification", notification);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+   // res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+   res.send("Server is running")
 })
 
 // Middleware for Errors
